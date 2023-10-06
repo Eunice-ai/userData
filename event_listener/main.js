d7f7a9d7f45997353b295517edd812057a6b1399
eunice_ai <eunyben4@gmail.com>
2023-10-06 16:04:06 +0100

    some work on eventListener updated

--- event_listener/main.js
+++ event_listener/main.js
@@ -141,12 +141,150 @@ const keyMappings = [
   { keyCode: 16, keyLabel: "Shift" },
   { keyCode: 17, keyLabel: "Ctrl" },
   { keyCode: 18, keyLabel: "Alt" },
+  { keyCode: 189, keyLabel: "-" }, // Hyphen/Minus
+  { keyCode: 187, keyLabel: "=" }, // Equal Sign
+  { keyCode: 219, keyLabel: "[" }, // Left Bracket
+  { keyCode: 221, keyLabel: "]" }, // Right Bracket
+  { keyCode: 220, keyLabel: "\\" }, // Backslash
+  { keyCode: 59, keyLabel: ";" },  // Semicolon
+  { keyCode: 222, keyLabel: "'" }, // Single Quote
+  { keyCode: 188, keyLabel: "," },  // Comma
+  { keyCode: 190, keyLabel: "." },  // Period
+  { keyCode: 191, keyLabel: "/" },  // Slash
+  { keyCode: 96, keyLabel: "0" },  // Numpad 0
+  { keyCode: 97, keyLabel: "1" },  // Numpad 1
+  { keyCode: 98, keyLabel: "2" },  // Numpad 2
+  { keyCode: 99, keyLabel: "3" },  // Numpad 3
+  { keyCode: 100, keyLabel: "4" }, // Numpad 4
+  { keyCode: 101, keyLabel: "5" }, // Numpad 5
+  { keyCode: 102, keyLabel: "6" }, // Numpad 6
+  { keyCode: 103, keyLabel: "7" }, // Numpad 7
+  { keyCode: 104, keyLabel: "8" }, // Numpad 8
+  { keyCode: 105, keyLabel: "9" }, // Numpad 9
+  { keyCode: 106, keyLabel: "*" }, // Numpad *
+  { keyCode: 107, keyLabel: "+" }, // Numpad +
+  { keyCode: 109, keyLabel: "-" }, // Numpad -
+  { keyCode: 110, keyLabel: "." },  // Numpad .
+  { keyCode: 111, keyLabel: "/" },  // Numpad /
+  { keyCode: 112, keyLabel: "F1" },
+  { keyCode: 113, keyLabel: "F2" },
+  { keyCode: 114, keyLabel: "F3" },
+  { keyCode: 115, keyLabel: "F4" },
+  { keyCode: 116, keyLabel: "F5" },
+  { keyCode: 117, keyLabel: "F6" },
+  { keyCode: 118, keyLabel: "F7" },
+  { keyCode: 119, keyLabel: "F8" },
+  { keyCode: 120, keyLabel: "F9" },
+  { keyCode: 121, keyLabel: "F10" },
+  { keyCode: 122, keyLabel: "F11" },
+  { keyCode: 123, keyLabel: "F12" },
+  { keyCode: 144, keyLabel: "Num Lock" },
+  { keyCode: 145, keyLabel: "Scroll Lock" },
+  { keyCode: 186, keyLabel: ";" },  // Semicolon (with Shift)
+  { keyCode: 187, keyLabel: "=" },  // Equal Sign (with Shift)
+  { keyCode: 188, keyLabel: "," },  // Comma (with Shift)
+  { keyCode: 189, keyLabel: "-" },  // Hyphen/Minus (with Shift)
+  { keyCode: 190, keyLabel: "." },  // Period (with Shift)
+  { keyCode: 191, keyLabel: "/" },  // Slash (with Shift)
+  { keyCode: 192, keyLabel: "`" },  // Grave Accent/Backtick
+  { keyCode: 219, keyLabel: "{" },  // Left Curly Brace (with Shift)
+  { keyCode: 220, keyLabel: "\\" }, // Backslash (with Shift)
+  { keyCode: 221, keyLabel: "}" },  // Right Curly Brace (with Shift)
+  { keyCode: 222, keyLabel: '"' },
+  { keyCode: 192, keyLabel: "`" },  // Grave Accent/Backtick (without Shift)
+  { keyCode: 219, keyLabel: "[" },  // Left Bracket (without Shift)
+  { keyCode: 220, keyLabel: "\\" }, // Backslash (without Shift)
+  { keyCode: 221, keyLabel: "]" },  // Right Bracket (without Shift)
+  { keyCode: 222, keyLabel: "'" },  // Single Quote (without Shift)
+  { keyCode: 223, keyLabel: "`" },  // Grave Accent/Backtick (with Shift)
+  { keyCode: 226, keyLabel: "\\" }, // Backslash (with AltGr)
+  { keyCode: 107, keyLabel: "+" },  // Plus Sign (Numpad with Num Lock)
+  { keyCode: 109, keyLabel: "-" },  // Minus Sign (Numpad with Num Lock)
+  { keyCode: 106, keyLabel: "*" },  // Asterisk (Numpad with Num Lock)
+  { keyCode: 111, keyLabel: "/" },  // Slash (Numpad with Num Lock)
+  { keyCode: 110, keyLabel: "." },  // Decimal Point (Numpad with Num Lock)
+
+  // Media control keys
+  { keyCode: 173, keyLabel: "Volume Mute" },
+  { keyCode: 174, keyLabel: "Volume Down" },
+  { keyCode: 175, keyLabel: "Volume Up" },
+  { keyCode: 176, keyLabel: "Media Next Track" },
+  { keyCode: 177, keyLabel: "Media Previous Track" },
+  { keyCode: 178, keyLabel: "Media Stop" },
+  { keyCode: 179, keyLabel: "Media Play/Pause" },
+
+  // Browser control keys
+  { keyCode: 181, keyLabel: "Volume Mute (Chrome)" },
+  { keyCode: 182, keyLabel: "Volume Down (Chrome)" },
+  { keyCode: 183, keyLabel: "Volume Up (Chrome)" },
+
+  // IME (Input Method Editor) keys
+  { keyCode: 229, keyLabel: "IME Hiragana/Katakana" },
+
+  // Web browser keys
+  { keyCode: 166, keyLabel: "Browser Back" },
+  { keyCode: 167, keyLabel: "Browser Forward" },
+  { keyCode: 168, keyLabel: "Browser Refresh" },
+  { keyCode: 169, keyLabel: "Browser Stop" },
+  { keyCode: 170, keyLabel: "Browser Search" },
+  { keyCode: 171, keyLabel: "Browser Favorites" },
+  { keyCode: 172, keyLabel: "Browser Home" },
+
+  // Additional keys (may vary by keyboard)
+  { keyCode: 183, keyLabel: "Launch Calculator" },
+  { keyCode: 184, keyLabel: "Launch Email" },
+  { keyCode: 91, keyLabel: "Left Windows Key (WinKey)" },
+  { keyCode: 92, keyLabel: "Right Windows Key (WinKey)" },
+  { keyCode: 93, keyLabel: "Menu Key (Application Key)" },
+
+  // Mac Command key
+  { keyCode: 224, keyLabel: "Left or Right Command (⌘) Key (Mac)" },
+
+  // Mac Option key
+  { keyCode: 225, keyLabel: "Left or Right Option (⌥) Key (Mac)" },
+
+  // Mac Control key
+  { keyCode: 17, keyLabel: "Control (Ctrl) Key (Mac)" },
+
+  // Mac Function keys (F13 - F15)
+  { keyCode: 183, keyLabel: "F13 (Mac)" },
+  { keyCode: 184, keyLabel: "F14 (Mac)" },
+  { keyCode: 185, keyLabel: "F15 (Mac)" },
+
+  // Multimedia keys (may vary by keyboard)
+  { keyCode: 173, keyLabel: "Media Volume Mute" },
+  { keyCode: 174, keyLabel: "Media Volume Down" },
+  { keyCode: 175, keyLabel: "Media Volume Up" },
+  { keyCode: 179, keyLabel: "Media Play/Pause" },
+  { keyCode: 178, keyLabel: "Media Stop" },
+  { keyCode: 177, keyLabel: "Media Previous Track" },
+  { keyCode: 176, keyLabel: "Media Next Track" },
+
+  // Lock keys (may vary by keyboard)
+  { keyCode: 20, keyLabel: "Caps Lock" },
+  { keyCode: 145, keyLabel: "Scroll Lock" },
+  { keyCode: 144, keyLabel: "Num Lock" },
+
+  // Special navigation keys
+  { keyCode: 45, keyLabel: "Insert" },
+  { keyCode: 46, keyLabel: "Delete" },
+  { keyCode: 36, keyLabel: "Home" },
+  { keyCode: 35, keyLabel: "End" },
+  { keyCode: 33, keyLabel: "Page Up" },
+  { keyCode: 34, keyLabel: "Page Down" }
 ];
 
+// result is wrong will get back to it later
 document.body.addEventListener('keypress', e => {
-
-  alert(`
-  you pressed ${e.key} key
-  ${e.code}
-  `)
-})
+ const key = e.key;
+  const keyMapping = keyMappings.find(mapping => mapping.keyLabel === key);
+  
+  if (keyMapping) {
+    const keyLabel = keyMapping.keyLabel;
+    
+    alert(`key pressed ${keyLabel} ${key}`)
+   } 
+  else {
+    alert(`you pressed: keycode: ${key}`)
+  }
+});
